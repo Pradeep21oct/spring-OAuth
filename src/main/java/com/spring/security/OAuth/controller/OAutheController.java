94f85a5844a1efe1cf40140e20b2f6f34365060d
Shreyashi Singh <shreyashi01.singh@gmail.com>
2025-03-02 22:07:11 -0500

    Oauthe message

---
+++ src/main/java/com/spring/security/OAuth/controller/OAutheController.java
@@ -0,0 +1,12 @@
+package com.spring.security.OAuth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAutheController {
+    @GetMapping("hello")
+    public String helloOAuth(){
+        return "WelCome OAuth2";
+    }
+}
