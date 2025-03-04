aebb4d902644cf197f90c869b320978c11618183
Shreyashi Singh <shreyashi01.singh@gmail.com>
2025-03-03 22:01:05 -0500

    Oauthe message

--- src/main/java/com/spring/security/OAuth/config/SecurityConfig.java
+++ src/main/java/com/spring/security/OAuth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.OAuth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,8 +13,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
-
-       return http.build();
+        return http.authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                 .build();
 
     }
 }
